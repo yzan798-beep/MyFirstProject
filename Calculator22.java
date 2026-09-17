df8da72429a442d3c300142e29b68ce79107266e
يزن مستور  محمد الكثيري <445050090@std.psau.edu.sa>
2026-09-17 09:16:29 +0300

    Add greeting feature

--- Calculator22.java
+++ Calculator22.java
@@ -6,6 +6,7 @@ public int subtract(int a, int b) {
 return a - b;
 }
 public static void main(String[] args) {
+    System.out.println("salam alikm");
 Calculator calc = new Calculator();
 System.out.println("5 + 3 = " + calc.add(5, 3));
 System.out.println("10 - 4 = " + calc.subtract(10, 4));
