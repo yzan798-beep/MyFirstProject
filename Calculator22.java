982ddc0ab9b44919dfb4dc74b726050e5ccaaf7b
يزن مستور  محمد الكثيري <445050090@std.psau.edu.sa>
2026-09-17 08:51:39 +0300

    Initial commit: Add Calculator22.java

---
+++ Calculator22.java
@@ -0,0 +1,13 @@
+public class Calculator22 {
+public int add(int a, int b) {
+return a + b;
+}
+public int subtract(int a, int b) {
+return a - b;
+}
+public static void main(String[] args) {
+Calculator calc = new Calculator();
+System.out.println("5 + 3 = " + calc.add(5, 3));
+System.out.println("10 - 4 = " + calc.subtract(10, 4));
+}
+}
