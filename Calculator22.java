46181fbcd6a217d007b093cd60303b8bebb34577
يزن مستور  محمد الكثيري <445050090@std.psau.edu.sa>
2026-09-17 09:38:11 +0300

    update to arabic

--- Calculator22.java
+++ Calculator22.java
@@ -6,7 +6,7 @@ public int subtract(int a, int b) {
 return a - b;
 }
 public static void main(String[] args) {
-    System.out.println("salam alikm");
+    System.out.println("السلام عليكم");
 Calculator calc = new Calculator();
 System.out.println("5 + 3 = " + calc.add(5, 3));
 System.out.println("10 - 4 = " + calc.subtract(10, 4));
